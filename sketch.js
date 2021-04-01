3d68cacae23f9b63f683d8e142f8ba12368d1034
stuteemohanty29 <72800475+stuteemohanty29@users.noreply.github.com>
2021-04-01 09:54:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,197 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
+var score=0;
+
+var gameOver, restart;
+
+localStorage["HighestScore"] = 0;
+
+function preload(){
+  mario_running =   loadAnimation("mario1.png","mario2.png","mario3.png","mario4.png","mario5.png")
+  background1 = loadImage("background 1.jpg");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("sheild .png");
+  
+  obstacle1 = loadImage("dragon1.png");
+  obstacle2 = loadImage("green ball.png");
+  obstacle3 = loadImage("coin1.png");
+  obstacle4 = loadImage("mushroom1 .png");
+  obstacle5 = loadImage("dragon1.png");
+  obstacle6 = loadImage("green ball.png");
+  
+  gameOverImg = loadImage("gameOver.png");
+  restartImg = loadImage("restart.png");
+} 
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  
+  mario= createSprite(50,180,20,50);
+  
+  mario.addAnimation("running", mario_running);
+  mario.addAnimation("collided", mario_running);
+  mario.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -(6 + 3*score/100);
+  
+  gameOver = createSprite(camera.position.x+50,camera.position.y);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(camera.position.x+50,camera.position.x+30);
+  restart.addImage(restartImg);
+  
+  gameOver.scale = 0.5;
+  restart.scale = 0.5;
+
+  gameOver.visible = false;
+  restart.visible = false;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  cloudsGroup = new Group();
+  obstaclesGroup = new Group();
+  
+  score = 0;
+}
+
+function draw() {
+  //trex.debug = true;
+  background(background1);
+  text("Score: "+ score, camera.position.x+250,camera.position.y-250);
+ 
+  camera.position.x=mario.x;
+  camera.position.y=mario.y;                  
+  if (gameState===PLAY){
+    score = score + Math.round(getFrameRate()/60);
+    ground.velocityX = -(6 + 3*score/100);
+  console.log(mario.y);
+    if(keyDown("space") && mario.y >= 119) {
+      mario.velocityY = -12;
+    }
+  
+    mario.velocityY =  mario.velocityY + 0.8;
+  
+    if (ground.x <camera.position.x){
+      ground.x =300;
+    }
+  
+    mario.collide(invisibleGround);
+    spawnClouds();
+    spawnObstacles();
+  
+    if(obstaclesGroup.isTouching(mario)){
+        gameState = END;
+    }
+  }
+  else if (gameState === END) {
+    gameOver.visible = true;
+    restart.visible = true;
+    
+    //set velcity of each game object to 0
+    ground.velocityX = 0;
+    mario.velocityY = 0;
+    obstaclesGroup.setVelocityXEach(0);
+    cloudsGroup.setVelocityXEach(0);
+    
+    //change the trex animation
+    mario.changeAnimation("collided",mario_running);
+    
+    //set lifetime of the game objects so that they are never destroyed
+    obstaclesGroup.setLifetimeEach(-1);
+    cloudsGroup.setLifetimeEach(-1);
+    
+    if(mousePressedOver(restart)) {
+      reset();
+    }
+  }
+  
+  
+  drawSprites();
+}
+
+function spawnClouds() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    var cloud = createSprite(600,120,40,10);
+    cloud.y = Math.round(random(80,120));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+     //assign lifetime to the variable
+    cloud.lifetime = 200;
+    
+    //adjust the depth
+    cloud.depth = mario.depth;
+    mario.depth = mario.depth + 1;
+    
+    //add each cloud to the group
+    cloudsGroup.add(cloud);
+  }
+  
+}
+
+function spawnObstacles() {
+  if(frameCount % 60 === 0) {
+    var obstacle = createSprite(600,165,10,40);
+    //obstacle.debug = true;
+    obstacle.velocityX = -(6 + 3*score/100);
+    
+    //generate random obstacles
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
+    }
+    
+    //assign scale and lifetime to the obstacle           
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+    //add each obstacle to the group
+    obstaclesGroup.add(obstacle);
+  }
+}
+
+function reset(){
+  gameState = PLAY;
+  gameOver.visible = false;
+  restart.visible = false;
+  
+  obstaclesGroup.destroyEach();
+  cloudsGroup.destroyEach();
+  
+  mario.changeAnimation("running",mario_running);
+  
+  if(localStorage["HighestScore"]<score){
+    localStorage["HighestScore"] = score;
+  }
+  console.log(localStorage["HighestScore"]);
+  
+  score = 0;
+  
+}
